98e5583d4cad2b8e269221388786b36996cd5457
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-08-27 11:38:46 +0200

    controller done

--- WebApi/Controllers/CustomersController.cs
+++ WebApi/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Interfaces;
+using WebApi.Models;
 
 namespace WebApi.Controllers;
 
@@ -9,4 +10,44 @@ namespace WebApi.Controllers;
 public class CustomersController(ICustomerService customerService) : ControllerBase
 {
     private readonly ICustomerService _customerService = customerService;
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCustomer(CustomerRequest customerRequest)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var customer = await _customerService.CreateCustomerAsync(customerRequest);
+        if (!customer.Succeeded)
+            return StatusCode(customer.StatusCode, customer.Error);
+
+        return StatusCode(customer.StatusCode, customer.Result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCustomers()
+    {
+        var customers = await _customerService.GetAllCustomersAsync();
+        
+        if (!customers.Succeeded)
+            return StatusCode(customers.StatusCode, customers.Error);
+        
+        return StatusCode(customers.StatusCode, customers.Result);
+    }
+
+
+    // GET api/customers/{customerId}
+    // GET api/customers?customerId=123
+
+    [HttpGet("{customerId}")]
+    public async Task<IActionResult> GetCustomerById(string customerId)
+    {
+        var customer = await _customerService.GetCustomerByIdAsync(customerId);
+        
+        if (!customer.Succeeded)
+            return StatusCode(customer.StatusCode, customer.Error);
+        
+        return StatusCode(customer.StatusCode, customer.Result);
+    }
 }
